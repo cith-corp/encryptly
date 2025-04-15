05834906d5361db67fd37b26f1d917a44a1da9fa
Gan Jeisbauer <foobar@zeroday.network>
2025-04-15 22:28:10 +0200

    Adjust css and main

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Theme } from "@radix-ui/themes";
+import "./index.css";
+import App from "./App";
+
+import "@radix-ui/themes/styles.css";
+
 createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-      <Theme appearance="dark">
-        <App />
-      </Theme>
-    </StrictMode>
-  );
+  <StrictMode>
+    <Theme appearance="dark">
+      <App />
+    </Theme>
+  </StrictMode>
+);

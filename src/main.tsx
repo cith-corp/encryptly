786330d1bc28670e4862c928055b2ad9b568ba8b
Scharco Meel <barfoo@zeroday.network>
2025-06-26 15:35:33 +0200

    add legal bullshit; change logo so we don't get sued

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,32 @@ import { createRoot } from "react-dom/client";
 import { Theme } from "@radix-ui/themes";
 import "./index.css";
 import App from "./App";
+import { RouterProvider, createHashRouter } from "react-router";
+
 
 import "@radix-ui/themes/styles.css";
+import PrivacyPolicy from "./PrivacyPolicy";
+import Imprint from "./Imprint";
+
+let router = createHashRouter([
+  {
+    path: "/",
+    Component: App
+  },
+  {
+    path: "/privacy",
+    Component: PrivacyPolicy
+  },
+  {
+    path: "/imprint",
+    Component: Imprint
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Theme appearance="dark">
-      <App />
+      <RouterProvider router={router}/>
     </Theme>
   </StrictMode>
 );
